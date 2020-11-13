99ab8e4365b2aec2a6e64db0ca3a4972a7cd0bd6
VASISHT,VARUN (K-India,ex1) <varun_vasisht@keysight.com>
2020-11-13 16:41:28 +0530

    Add project files.

---
+++ AwsMessageQueueTests/AmazonSQSTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AwsMessageQueueTests
+{
+  [TestClass]
+  public class AmazonSQSTests
+  {
+    [TestMethod]
+    public void TestCreateMessageQueue()
+    {
+      AmazonSQSConfig SqsConfig = new AmazonSQSConfig
+      {
+        ServiceURL = "http://sqs.us-east-2.amazonaws.com"
+      };
+
+      AmazonSQSClient SqsClient = new AmazonSQSClient(SqsConfig);
+      //QueueUrl = "https://sqs.us-east-2.amazonaws.com/330261344570/TestingTheQueue";
+
+      var createQueueRequest = new CreateQueueRequest();
+      createQueueRequest.QueueName = "TestingQueue123";
+
+      var createQueueResponse = SqsClient.CreateQueue(createQueueRequest);
+
+      Assert.IsNotNull(createQueueResponse);
+    }
+
+
+    [TestMethod]
+    public void TestSendTheMessageToQueue()
+    {
+      AmazonSQSConfig SqsConfig = new AmazonSQSConfig
+      {
+        ServiceURL = "http://sqs.us-east-2.amazonaws.com"
+      };
+
+      AmazonSQSClient SqsClient = new AmazonSQSClient(SqsConfig);
+      string QueueUrl = "https://sqs.us-east-2.amazonaws.com/330261344570/TestingTheQueue";
+
+      // sending the message to the message queue  
+      var sendMessageRequest = new SendMessageRequest
+      {
+        QueueUrl = QueueUrl,
+        MessageBody = "This is a simple message queue test"
+      };
+      var sendMessageResponse = SqsClient.SendMessage(sendMessageRequest);
+
+      Assert.IsNotNull(sendMessageResponse);
+    }
+  }
+}
+
---
+++ AwsMessageQueueTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AwsMessageQueueTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AwsMessageQueueTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c5c4f38c-e028-4aa9-abc7-a262fc924847")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
